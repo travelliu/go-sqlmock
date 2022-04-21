5822a44ac7a203179507f5916644798ae42a8860
travelliu <bin.liu@enmotech.com>
2022-04-21 11:34:41 +0800

    feat: add godror option args

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/DATA-DOG/go-sqlmock
 
 go 1.15
 
-require github.com/kisielk/sqlstruct v0.0.0-20201105191214-5f3e10d3ab46
+require (
+	github.com/godror/godror v0.33.0
+	github.com/kisielk/sqlstruct v0.0.0-20201105191214-5f3e10d3ab46
+)
--- sqlmock_go19.go
+++ sqlmock_go19.go
@@ -1,3 +1,4 @@
+//go:build go1.9
 // +build go1.9
 
 package sqlmock
@@ -5,12 +6,15 @@ package sqlmock
 import (
 	"database/sql"
 	"database/sql/driver"
+	"github.com/godror/godror"
 )
 
 // CheckNamedValue meets https://golang.org/pkg/database/sql/driver/#NamedValueChecker
 func (c *sqlmock) CheckNamedValue(nv *driver.NamedValue) (err error) {
 	switch nv.Value.(type) {
 	case sql.Out:
+		return nil
+	case godror.Option:
 		return nil
 	default:
 		nv.Value, err = c.converter.ConvertValue(nv.Value)
